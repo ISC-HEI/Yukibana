eb886ebe804629e8a885c1fe0bc2942888de352b
LordBaryhobal <louis.heredero@hevs.ch>
2026-09-18 10:27:52 +0200

    refactor: make frontend cleanup more modular

--- theia-extensions/yukibana/src/browser/yukibana-cleanup-contribution.ts
+++ theia-extensions/yukibana/src/browser/yukibana-cleanup-contribution.ts
@@ -5,15 +5,24 @@ import { MaybePromise } from '@theia/core';
 import { FrontendApplication, FrontendApplicationContribution, Widget } from '@theia/core/lib/browser';
 import { injectable } from '@theia/core/shared/inversify';
 
+type WidgetFilter = (id: string) => boolean;
+
 /**
  * Contribution to remove some frontend elements
  */
 @injectable()
 export class CleanupFrontendContribution implements FrontendApplicationContribution {
+    private _widgetsToRemove: Array<WidgetFilter> = [
+        id => id.startsWith('terminal'),
+        id => id.endsWith('metals-explorer'),
+    ];
+
     onDidInitializeLayout(app: FrontendApplication): MaybePromise<void> {
         app.shell.widgets.forEach((widget: Widget) => {
-            if (widget.id.startsWith('terminal')) {
-                widget.dispose();
+            for (const filter of this._widgetsToRemove) {
+                if (filter(widget.id)) {
+                    widget.dispose();
+                }
             }
         });
     }
